dd5e1dbafc6f25b843b3f1edef5bde4538cc5144
aklyussef <aklyussef@gmail.com>
2019-05-06 01:27:17 +0200

    Fixed syntax errors not caught by vim and added TODOs

--- ModelData.py
+++ ModelData.py
@@ -1,6 +1,7 @@
 import os
 #Load librariestry:
 import pandas
+import numpy as np
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
@@ -16,65 +17,65 @@ from sklearn.svm import SVC
 
 class ModelData:
 
+    #TODO: Figure out how to change data into binary classification problem using threshold since this class only handles bianry/multiclass classification
+
     def __init__(self,filepath):
         self.dataset = pandas.read_csv(filepath)
         self.labels = list(self.dataset.columns)
-	#TODO: Accept splits from ModelData constructor
-	self.splits = 5
-        pass
+        #TODO: Accept splits from ModelData constructor
+        self.splits = 2
+        return
 
-    def show_data_features():
+    def show_data_features(self):
         #shape
-        print(dataset.shape)
-
+        print(self.dataset.shape)
         #head
-        print(dataset.head(20))
-
+        print(self.dataset.head(20))
         #statistical info on dataset
-        print(dataset.describe())
-
+        print(self.dataset.describe())
         #class distribution
-        print(dataset.groupby(self.labels[-1]).size())
-        pass
+        print(self.dataset.groupby(self.labels[-1]).size())
+        return
 
     def fit_and_evaluate(self):
-        array = dataset.values
-        X = array[:,0:len(self.labels)-1]
+        array = self.dataset.values
+        X = array[:,1:len(self.labels)-1]
         Y = array[:,len(self.labels)-1]
         validation_size = 0.20
         seed = 7
-	scoring = 'accuracy'
-	
-	self.models = []	
-	self.models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-	self.models.append(('LDA', LinearDiscriminantAnalysis()))
-	self.models.append(('KNN', KNeighborsClassifier()))
-	self.models.append(('CART', DecisionTreeClassifier()))
-	self.models.append(('NB', GaussianNB()))
-	self.models.append(('SVM', SVC(gamma='auto')))
-	# evaluate each model in turn
-	self.results = []
-	self.names = []
-	for name, model in self.models:
-		kfold = model_selection.KFold(n_splits=self.splits, random_state=seed)
-		cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-		self.results.append(cv_results)
-		self.names.append(name)
-		msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	return models
+        X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                        random_state=seed)
+        scoring = 'accuracy'
+
+        self.models = []
+        self.models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+        self.models.append(('LDA', LinearDiscriminantAnalysis()))
+        self.models.append(('KNN', KNeighborsClassifier()))
+        self.models.append(('CART', DecisionTreeClassifier()))
+        self.models.append(('NB', GaussianNB()))
+        self.models.append(('SVM', SVC(gamma='auto')))
+        # evaluate each model in turn
+        self.results = []
+        self.names = []
+        for name, model in self.models:
+            kfold = model_selection.KFold(n_splits=self.splits, random_state=seed)
+            cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+            self.results.append(cv_results)
+            self.names.append(name)
+            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 
 
-    def show_comparison(self):
-	fig = plt.figure()
-	fig.suptitle('Algorithm Comparison')
-	ax = fig.add subplot(111)
-	plt.boxplot(self.results)	
-	ax.set_xticklabels(self.names)
-	plt.show()
+        def show_comparison(self):
+            fig = plt.figure()
+            fig.suptitle('Algorithm Comparison')
+            ax = fig.add_subplot(111)
+            plt.boxplot(self.results)
+            ax.set_xticklabels(self.names)
+            plt.show()
 
 
 def main():
-    md = ModelData('../data/output/network_results.csv')
+    md = ModelData('../data/output/network_summary.csv')
     md.show_data_features()
     md.fit_and_evaluate()
 
