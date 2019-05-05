98b67167b72e03b366a76a6d3980ad8c1b8a41bd
aklyussef <aklyussef@gmail.com>
2019-05-05 15:23:47 +0200

    Integrated code into ModelData class Need to test with a running environment to make sure it's working fine and figure out how to integrate prediction code

--- ModelData.py
+++ ModelData.py
@@ -19,6 +19,8 @@ class ModelData:
     def __init__(self,filepath):
         self.dataset = pandas.read_csv(filepath)
         self.labels = list(self.dataset.columns)
+	#TODO: Accept splits from ModelData constructor
+	self.splits = 5
         pass
 
     def show_data_features():
@@ -35,62 +37,55 @@ class ModelData:
         print(dataset.groupby(self.labels[-1]).size())
         pass
 
-    def split_data(self):
+    def fit_and_evaluate(self):
         array = dataset.values
         X = array[:,0:len(self.labels)-1]
         Y = array[:,len(self.labels)-1]
         validation_size = 0.20
         seed = 7
+	scoring = 'accuracy'
+	
+	self.models = []	
+	self.models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+	self.models.append(('LDA', LinearDiscriminantAnalysis()))
+	self.models.append(('KNN', KNeighborsClassifier()))
+	self.models.append(('CART', DecisionTreeClassifier()))
+	self.models.append(('NB', GaussianNB()))
+	self.models.append(('SVM', SVC(gamma='auto')))
+	# evaluate each model in turn
+	self.results = []
+	self.names = []
+	for name, model in self.models:
+		kfold = model_selection.KFold(n_splits=self.splits, random_state=seed)
+		cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+		self.results.append(cv_results)
+		self.names.append(name)
+		msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	return models
+
+
+    def show_comparison(self):
+	fig = plt.figure()
+	fig.suptitle('Algorithm Comparison')
+	ax = fig.add subplot(111)
+	plt.boxplot(self.results)	
+	ax.set_xticklabels(self.names)
+	plt.show()
+
 
 def main():
-    print('main')
+    md = ModelData('../data/output/network_results.csv')
+    md.show_data_features()
+    md.fit_and_evaluate()
 
 if __name__ == '__main__':
     main()
 
-#split-out validation dataset
-array = dataset.values
-X = array[:,0:4]
-Y= array[:,4]
-validation_size = 0.20
-seed = 7
-X_train,X_validation,Y_train,Y_validation = model_selection.train_test_split(X,Y,test_size=validation_size,random_state=seed)
-#Test options and evaluation metric
-#seed = 7
-scoring = 'accuracy'
-
-# Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
-
-
-# Compare Algorithms
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
-
-# Make predictions on validation dataset
-knn = KNeighborsClassifier()
-knn.fit(X_train, Y_train)
-predictions = knn.predict(X_validation)
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
+def to_be_integrated():
+	# Make predictions on validation dataset
+	knn = KNeighborsClassifier()
+	knn.fit(X_train, Y_train)
+	predictions = knn.predict(X_validation)
+	print(accuracy_score(Y_validation, predictions))
+	print(confusion_matrix(Y_validation, predictions))
+	print(classification_report(Y_validation, predictions))
